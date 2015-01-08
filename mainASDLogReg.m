f01c6a63873de7de68b32a529431ee7d14283c18
mobeets <mobeets@gmail.com>
2015-01-08 14:53:16 -0600

    fixing mapfcn

--- mainASDLogReg.m
+++ mainASDLogReg.m
@@ -30,7 +30,9 @@ scores = reg.scoreCVGrid(X_train, Y_train, X_test, Y_test, mapFcn, scoreFcn, nfo
 % function [mu, b] = mapFcn(X_train, Y_train, hyper, opts)
 %     fitIntercept = true;    
 %     D = opts.D;
+%     ssq = hyper(2);
 %     [X, Y, X_mean, Y_mean] = reg.centerData(X_train, Y_train, fitIntercept);
-%     [mu, Reg, hyper] = asd.asd(X, Y, D, nan, true);
+%     [XX, XY, YY, p, q, Reg] = asd.init(X, Y, D, hyper);
+%     [mu, ~] = tools.meanInvCov(XX, XY, Reg, ssq);
 %     b = reg.setIntercept(X_mean, Y_mean, mu, fitIntercept);
 % end
