cd99682fe4382330ef87c41694b03ef3f6671bf4
Jay <mobeets@gmail.com>
2015-05-05 14:18:20 -0500

    bern regression should not fit intercept

--- +ml/fitHandle.m
+++ +ml/fitHandle.m
@@ -1,14 +1,15 @@
 function opts = fitHandle(llstr)    
     opts.hyperFcn = @(X, Y, hyper) hyper;
     opts.hyperFcnArgs = {nan};
+    opts.fitIntercept = true;
     if strcmp(llstr, 'gauss')
         opts.muFcn = @ml.calcGaussML;
     elseif strcmp(llstr, 'bern')
         opts.muFcn = @ml.calcBernML;
+        opts.fitIntercept = false; % only fitting 0/1
     elseif strcmp(llstr, 'poiss')
         opts.muFcn = @ml.calcPoissML;
     end
-    opts.muFcnArgs = {};
-    opts.fitIntercept = true;
+    opts.muFcnArgs = {};    
     opts.centerX = false;
 end
