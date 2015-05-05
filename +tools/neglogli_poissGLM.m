e38d14eda64e55c4f02389070a2d1c04f962c738
Jay <mobeets@gmail.com>
2015-05-05 13:24:39 -0500

    pseudo r-sq fixed to have appropriate saturated model; adding pctCorrect as score fcn in logistic reg

--- +tools/neglogli_poissGLM.m
+++ +tools/neglogli_poissGLM.m
@@ -1,13 +1,13 @@
 function [L,dL,H] = neglogli_poissGLM(wts,x,y,fnlin)
-% [L,dL,ddL] = neglogli_poissGLM(wts,X,Y)
+% [L,dL,ddL] = neglogli_poissGLM(wts,x,y,fnlin)
 %
 % Compute negative log-likelihood of data under Poisson regression model,
 % plus gradient and Hessian
 %
 % INPUT:
 % wts [m x 1] - regression weights
-%   X [N x m] - regressors
-%   Y [N x 1] - output (binary vector of 1s and 0s).
+%   x [N x m] - regressors
+%   y [N x 1] - output (binary vector of 1s and 0s).
 %       fnlin - func handle for nonlinearity (must return f, df and ddf)
 %
 % OUTPUT:
@@ -15,35 +15,31 @@ function [L,dL,H] = neglogli_poissGLM(wts,x,y,fnlin)
 %  dL [m x 1] - gradient
 % ddL [m x m] - Hessian
 
-m = numel(wts);
 xproj = x*wts;
+
+% if any are -Inf or 0, log() will blow up
+xproj((xproj < 0) & isinf(xproj)) = -1e9;
+xproj(xproj == 0) = 1e-9;
+
+Lf = @(y, f) -y'*log(f) + sum(f); % neg log-likelihood
+dLf = @(x, y, f, df) x'*((1 - y./f) .* df); % gradient
+Hf = @(x, y, f, df, ddf) bsxfun(@times, ddf.*(1-(y./f)) + ...
+    df.*(y./f.^2.*df), x)'*x;
 
 switch nargout
     case 1
-	f = fnlin(xproj);
-	L = -y'*log(f) + sum(f); % neg log-likelihood
-    L = full(L);
+        f = fnlin(xproj);
+        L = full(Lf(y, f));
     case 2
-	[f,df] = fnlin(xproj); % evaluate nonlinearity
-	
-	L = -y'*log(f) + sum(f); % neg log-likelihood
-    L = full(L);
-	%L(1 + (1:m)) = (1 - y./f)' * df * wts;
-	dL = x'*((1 - y./f) .* df);% K: fixed the gradient
-    dL = full(dL);
+        [f,df] = fnlin(xproj); % evaluate nonlinearity
+        L = full(Lf(y, f));
+        dL = full(dLf(x, y, f, df));
     case 3
-	[f,df,ddf] = fnlin(xproj); % evaluate nonlinearity
-
-	L = -y'*log(f) + sum(f); % neg log-likelihood
-	yf = y./f;
-	%L(1 + (1:m)) = (1 - yf)' * df * wts;
-	dL = x'*((1 - yf) .* df);
-	%H = (yf' * (df.^2 ./ f - ddf) + sum(ddf)) * (wts*wts');
-    H = bsxfun(@times,ddf.*(1-yf)+df.*(y./f.^2.*df) ,x)'*x; %K: fixed the Hessian
-    L = full(L);
-    dL = full(dL);
-    H = full(H);
-% 	L(m+2:end) = H(:);
+        [f,df,ddf] = fnlin(xproj); % evaluate nonlinearity
+        L = full(Lf(y, f));
+        dL = full(dLf(x, y, f, df));
+%         H = bsxfun(@times,ddf.*(1-(y./f))+df.*(y./f.^2.*df) ,x)'*x; %K: fixed the Hessian
+        H = full(Hf(x, y, f, df, ddf));
 end
 
 end % neglogli_poissGLM_sub
--- +tools/pseudoRsq.m
+++ +tools/pseudoRsq.m
@@ -2,10 +2,16 @@ function val = pseudoRsq(ll, llSat, llNull)
     if numel(ll) > 1 || numel(llSat) > 1 || numel(llNull) > 1
         x=1;
     end
+    if any(isnan([ll llSat llNull]))
+        x=1;
+    end    
     if llSat < llNull
         x=1;
     end
     if llSat < ll
+        x=1;
+    end
+    if sign(llSat - ll)*sign(llSat - llNull) < 0
         x=1;
     end
     assert(llSat >= llNull);
