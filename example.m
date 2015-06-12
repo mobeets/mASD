6764aad38f889bf7d5b6844512c01506d28e7b1b
Jay H <mobeets@gmail.com>
2015-06-12 10:49:41 -0500

    adding example.png

--- example.m
+++ example.m
@@ -1,17 +1,22 @@
-% rng(110);
+% To generate example.png:
+% 1. Get plotKernelSingle from https://github.com/mobeets/gaborMotionPulses
+% 2. Uncomment the rng line below to set seed
+% 3. Uncomment the plot lines
+
+% rng(101);
 
 %% truth
 
+Xxy = tools.gridCartesianProduct([0 0], [2 2], [5 5]);
+nw = size(Xxy, 1);
 nt = 1000;
-nw = 20;
-Xxy = randn(nw,2);
+
 D = asd.sqdist.space(Xxy);
 w = mvnrnd(zeros(nw,1), asd.prior(2, D, 1))';
-% figure(1); clf; plot.plotKernelSingle(Xxy, w, nan, 50); title('truth');
 
-X = rand(nt,nw);
+X = rand(nt,nw)-1;
 e = randn(nt,1);
-Y = X*w + 2*e;
+Y = X*w + 3*e;
 isLinReg = true;
 
 %% ASD
@@ -20,7 +25,6 @@ clear obj
 scoreObj = reg.getScoreObj(isLinReg, 'rsq');
 ASD = reg.getObj_ASD(X, Y, D);
 ASD = reg.fitAndScore(X, Y, ASD, scoreObj);
-% figure(2); clf; plot.plotKernelSingle(Xxy, ASD.w, nan, 50); title('ASD');
 
 %% ML
 
@@ -28,10 +32,15 @@ clear obj
 scoreObj = reg.getScoreObj(isLinReg, 'rsq');
 ML = reg.getObj_ML(X, Y);
 ML = reg.fitAndScore(X, Y, ML, scoreObj);
-% figure(3); clf; plot.plotKernelSingle(Xxy, ML.w, nan, 50); title('ML');
 
 %%
 
+% figure(1); clf; axis off; title('truth');
+% plot.plotKernelSingle(Xxy, w, nan, 130);
+% figure(2); clf; axis off; title('ASD');
+% plot.plotKernelSingle(Xxy, ASD.w, nan, 130);
+% figure(3); clf; axis off; title('ML');
+% plot.plotKernelSingle(Xxy, ML.w, nan, 130);
 % figure(4); clf; hold on;
 % f = reg.getPredictionFcn(isLinReg);
 % scatter(Y, Y-f(X,ASD.mu), 'b');
